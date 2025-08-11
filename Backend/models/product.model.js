8059d5d1803fe977a5ff528f8cf069fca713e5b0
Jasmeensingh98 <jasmeensingh264@gmail.com>
2025-08-11 11:30:28 +0530

    [Backend] - 3/august/2025 - setting up the product.controller.js , 5/august/2025 -  setting up the user.model.js , auth.route.js

--- Backend/models/product.model.js
+++ Backend/models/product.model.js
@@ -55,6 +55,7 @@ const ProductSchema = mongoose.Schema({
     },
   ],
 });
+ProductSchema.index({"$**":"text"})
 
 const Product = mongoose.model("Product", ProductSchema);
 export default Product;
---
+++ Backend/routes/product.route.js
@@ -0,0 +1,31 @@
+import {
+  ratingProduct,
+  getAllproducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controller/product.controller.js";
+import express from "express";
+const router = express.Router();
+
+// Rating product route
+
+router.put("/rating/:productId", ratingProduct);
+// get All products
+router.get("/", getAllproducts);
+// get one product
+
+router.get("/find:id", getProduct);
+
+//create product
+router.post("/", createProduct);
+//update product
+
+router.put("/:id", updateProduct);
+
+//delete product
+
+router.delete("/:id", deleteProduct);
+
+export default router;
