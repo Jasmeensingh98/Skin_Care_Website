dbb19db288a69d397c34a678ba244e7d84a59cbf
Jasmeensingh98 <jasmeensingh264@gmail.com>
2025-08-29 12:24:48 +0530

    [BackGroundServices] - implementing the delivery and promotion email for the user

--- BackgroundServices/index.js
+++ BackgroundServices/index.js
@@ -4,6 +4,8 @@ import dbConnection from "./utils/db.js";
 import cron from "node-cron";
 import sendWelcomeEmail from "./EmailServices/sendWelcomeEmail.js";
 import sendPendingOrderEmail from "./EmailServices/sendPendingOrderEmail.js";
+import sendDeliveredOrderEmail from "./EmailServices/sendDeliveredOrderEmail.js";
+import sendPromotionEmail from "./EmailServices/sendPromotionemail.js";
 
 dotenv.config();
 const app = express();
@@ -14,9 +16,18 @@ const services = () => {
   cron.schedule("* * * * *", () => { // every minute instead of every second
     sendWelcomeEmail();
     sendPendingOrderEmail();
+    sendDeliveredOrderEmail();
+  });
+};
+const promotion = () => {
+  cron.schedule("30 5 * * 5", () => { // every minute instead of every second
+   //sending promotion email 
+   sendPromotionEmail();
   });
 };
 
+services();
+promotion();
 const startServer = async () => {
   try {
     await dbConnection(); // connect first
