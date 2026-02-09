2a715a73f84f50f62405ee0e05d276da164e4330
jasmeen singh <jasmeensingh264@gmail.com>
2026-02-09 15:43:13 +0530

    Update the dependencies of package.json

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,8 @@ import dbConnection from "./util/db.js";
 dotenv.config();
 
 //server
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port  ${PORT}`);
   dbConnection();
